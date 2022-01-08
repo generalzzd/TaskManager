db4f9852c227f5ca27f55fba93781767d504f580
zidi <zidi@outlook.com>
2022-01-09 00:41:24 +0800

    update

--- mongodb.js
+++ mongodb.js
@@ -1,8 +1,16 @@
-console.log('This is mongodb script')
-
+var util= require('util');
+var encoder = new util.TextEncoder('utf-8');
 const mongodb = require('mongodb')
-const dbClient = mongodb.MongoClient
+
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const database = 'task-manager'
 
+const dbClient = mongodb.MongoClient
+dbClient.connect(connectionURL, {useNewUrlParser: true},(error, client)=>{
+    if(error)
+    {
+        return console.log('Unable to connect to database');
+    }
+    console.log("Connected Mongodb")
+})
