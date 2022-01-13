c33434dd6faa1b7a1345c92d533b638e05cf360e
zhangzidi <zhangzidi@BJ-94HC4Y2-505>
2022-01-13 14:29:36 +0800

    add blowfish

---
+++ src/test.js
@@ -0,0 +1,13 @@
+const bcrypt = require('bcryptjs')
+
+const myFunction = async () => {
+    const password = 'Red12345!'
+    const hashedPassword = await bcrypt.hash(password, 8)
+    console.log(hashedPassword)
+
+
+    const isMatch = await bcrypt.compare('Red12345!', hashedPassword)
+    console.log(isMatch)
+}
+
+myFunction()
