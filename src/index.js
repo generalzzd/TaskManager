232f2c93810acfc4ef6cbefd74ba7e9e664d23cb
zhangzidi <zhangzidi@BJ-94HC4Y2-505>
2022-01-13 13:43:37 +0800

    checkpoint

--- src/index.js
+++ src/index.js
@@ -102,6 +102,17 @@ app.patch('/task/:id', async (req, res) => {
 
 })
 
+app.delete('/task/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+        if(!task)
+            return res.status(404).send()
+        res.send(task)
+    } catch (error) {
+        res.status(500).send()        
+    }
+})
+
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
