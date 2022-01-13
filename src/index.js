ccbafbf14f0eabcba1b69327d0c8a1799ad7c2c1
zhangzidi <zhangzidi@BJ-94HC4Y2-505>
2022-01-13 12:32:43 +0800

    checkpoint

--- src/index.js
+++ src/index.js
@@ -8,37 +8,71 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     const user = new User(req.body)
 
-    user.save().then(() => {
-        res.send(user)
-    }).catch((e) => {
-        res.status(400).send(e)
-    })
+    // user.save().then(() => {
+    //     res.send(user)
+    // }).catch((e) => {
+    //     res.status(400).send(e)
+    // })
+    try {
+        await user.save()
+        res.status(201).send(user)
+    } catch (error) {
+        res.status(400).send(error)
+    }
 })
 
-app.get('/users', (req, res) => {
+app.get('/users', async (req, res) => {
 
-    User.find({}).then((users) => {
+    // User.find({}).then((users) => {
+    //     res.send(users)
+    // }).catch((e) => {
+    //     res.status(500).send(e)
+    // })
+
+    try {
+        const users = await User.find({})
         res.send(users)
-    }).catch((e) => {
-        res.status(500).send(e)
-    })
-
+    } catch (error) {
+        res.status(500).send(error)
+    }
 })
 
-app.get('/users/:id', (req, res) => {
-    const _id = req.params.id
+// app.get('/users/:id',async (req, res) => {
+//     const _id = req.params.id
 
-    User.findById(_id).then((user) => {
+//     User.findById(_id).then((user) => {
+//         if (!user) {
+//             return res.status(404).send()
+//         }
+//         res.send(user)
+//     }).catch((e) => {
+//         res.status(500).send()
+//     })
+// })
+
+app.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+    console.log('users patch')
+    if (!isValidOperation) {
+        return res.status(400).send({ error: "Invalid updates!" })
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+
         if (!user) {
             return res.status(404).send()
         }
+
         res.send(user)
-    }).catch((e) => {
-        res.status(500).send()
-    })
+    } catch (e) {
+        res.status(400).send(e)
+    }
 })
 
 app.post('/task', (req, res) => {
